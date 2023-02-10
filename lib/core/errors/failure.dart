14cb4fd640f833af6119eb36e637506f7ccedf10
Muhammad Salah <47930253+msalahii@users.noreply.github.com>
2023-02-10 19:49:27 +0200

    updates

--- lib/core/errors/failure.dart
+++ lib/core/errors/failure.dart
@@ -5,23 +5,3 @@ abstract class Failure extends Equatable {
 
   const Failure([List properties = const <dynamic>[]]);
 }
-
-class ServerFailure extends Failure {
-  final String failureMessage;
-
-  const ServerFailure({required this.failureMessage});
-
-  @override
-  String get message => failureMessage;
-
-  @override
-  List<Object?> get props => [];
-}
-
-class InternetFailure extends Failure {
-  @override
-  String get message => "Please check your internet connection!";
-
-  @override
-  List<Object?> get props => [];
-}
